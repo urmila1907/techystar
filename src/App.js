4defa2d08d547b34bef621234a07c344fde16b81
urmila1907 <urmilasangwan744@gmail.com>
2023-07-08 16:38:56 +0530

    added and styled footer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react';
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 import "./styles/App.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
+import "./styles/footer.scss";
 
 function App() {
   return (
@@ -14,11 +16,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home/>} />
-          {/* <Route path="/about" element={About} />
-          <Route path="/guides" element={Guides} />
-          <Route path="/project" element={ProjectIdeas} />
-          <Route path="/contact" element={ContactInfo} /> */}
         </Routes>
+        <Footer/>
       </Router>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Techystar</h1>
+        <p>@All rights reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="http://youtube.com" target={'blank'}>Youtube</a>
+          <a href="http://instagram.com" target={'blank'}>Instagram</a>
+          <a href="http://twitter.com" target={'blank'}>Twitter</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer
