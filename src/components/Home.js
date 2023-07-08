8a6f6f500c0085df4d428f13581d0a5ce9749dc0
urmila1907 <urmilasangwan744@gmail.com>
2023-07-08 11:38:37 +0530

    added home2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import vg from '../assets/5.webp'
 
 const Home = () => {
   return (
-    <div className='home'>
-    <main>
-        <h1>Tech deity</h1>
-        <p>Solution to all your tech issues</p>
-    </main>
-    </div>
-  )
+    <>
+      <div className="home">
+        <main>
+          <h1>Tech deity</h1>
+          <p>Solution to all your tech issues</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+        <div>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus excepturi mollitia, quisquam delectus id saepe. Illum omnis aut accusamus fugit? Culpa quo eveniet consequuntur hic fugiat blanditiis dolore velit ipsa.</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home
