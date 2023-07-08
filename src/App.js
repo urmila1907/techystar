9d85b3a9e28e9efc7534ff5d62becfe57ccec3a2
urmila1907 <urmilasangwan744@gmail.com>
2023-07-08 07:09:44 +0530

    made the initial changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route/>
+        </Routes>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
