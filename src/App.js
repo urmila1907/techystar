5cc1624b0251381553c3164f4b13e02645d33589
urmila1907 <urmilasangwan744@gmail.com>
2023-07-08 08:06:15 +0530

    Added scss files and a header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route/>
         </Routes>
